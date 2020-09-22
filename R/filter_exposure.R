a7e06258ab1b21f7ff7222a615566001d5f2dcb4
vkarhune <vkarhune@gmail.com>
2020-09-22 13:27:46 +0100

    Dependencies, documentations

--- R/filter_exposure.R
+++ R/filter_exposure.R
@@ -1,3 +1,8 @@
+#' @import data.table
+#' @import ieugwasr
+#' @import MendelianRandomization
+
+
 #' @export
 filter_exposure <- function(d = d_exposure, pthresh = pthresh_exposure){
   d_out <- d[pval < pthresh_exposure,]
--- R/proxies.R
+++ R/proxies.R
@@ -1,5 +1,15 @@
-#' @export
+#' Search for proxies
+#' 
+#' Function to search proxies for instrumental variables not found in the outcome summary statistics.
+#' Not implemented yet.
+#' 
+#' @param r2 $r^2$ parameter for proxies
+#'
+#' @param d_input Input data
+#'
+
 proxysearch <- function(r2 = proxy_r2, d_input = d_mr){
+  
   if(proxy_r2 < 0){
     cat(sprintf("No proxysearch\n"))
     d_out <- d_input
