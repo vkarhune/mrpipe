ab453418cbb7f3ca001ade4759b570f27ad87232
vkarhune <vkarhune@gmail.com>
2020-09-25 20:17:09 +0100

    Fix read_key() function

--- R/read_key.R
+++ R/read_key.R
@@ -4,6 +4,6 @@
 read_key <- function(file = NULL, chr = NULL){
   # load(file)
   # out <- get(paste0("chr", chr))
-  out <- readRDS(paste0(file, "chr", x, ".Rds"))
+  out <- readRDS(paste0(file, "chr", chr, ".Rds"))
   return(out)
 }
