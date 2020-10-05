254bba456fd54579ed7f059c44ca7b2870c41a38
vkarhune <vkarhune@gmail.com>
2020-10-05 09:49:47 +0100

    Fix read_file

--- R/read_file.R
+++ R/read_file.R
@@ -7,17 +7,20 @@ read_file <- function(file,
   
   if(tools::file_ext(file) %in% "Rds"){
     d_out <- readRDS(file)
-  } else if(is_nealelab){
-      d_out <- fread(cmd = paste0("zcat ", file), check.names = T)
-      # d_outcome[,"rsid" := fread(cmd = paste0("zcat ../misc/sumstats/variants.tsv.bgz"), check.names = T)$rsid]
-      variant_file <- ifelse(testing, "../misc/sumstats/variants.tsv.bgz", "data/variants.tsv.bgz")
-      d_out[,"rsid" := fread(cmd = paste0("zcat ", variant_file), check.names = T)$rsid]
-      d_out[,c("EA", "NEA") := lapply(4:3, function(x) sapply(strsplit(variant, ":"), "[[", x))]
-      d_out <- d_out[!(low_confidence_variant),]
-    # } else if(phenotype %in% "CRP"){
-    #  d_out <- readRDS(file)
+  } else if(is_nealelab | tools::file_ext(file) %in% "zip"){
+    d_out <- fread(cmd = paste0("zcat ", file), check.names = T)
   } else {
     d_out <- fread(file, check.names = T, nrows = ifelse(testing, 10000, Inf))
+  }
+  
+  if(is_nealelab){
+    # d_outcome[,"rsid" := fread(cmd = paste0("zcat ../misc/sumstats/variants.tsv.bgz"), check.names = T)$rsid]
+    variant_file <- ifelse(testing, "../misc/sumstats/variants.tsv.bgz", "data/variants.tsv.bgz")
+    d_out[,"rsid" := fread(cmd = paste0("zcat ", variant_file), check.names = T)$rsid]
+    d_out[,c("EA", "NEA") := lapply(4:3, function(x) sapply(strsplit(variant, ":"), "[[", x))]
+    d_out <- d_out[!(low_confidence_variant),]
+    # } else if(phenotype %in% "CRP"){
+    #  d_out <- readRDS(file)
   }
   
   if(phenotype %in% c("BMI")){
