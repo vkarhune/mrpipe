0e00142c7f52f5a942df00178fa500403cfbda64
vkarhune <vkarhune@gmail.com>
2020-10-09 00:17:37 +0100

    Fix R2()

--- R/R2.R
+++ R/R2.R
@@ -1,6 +1,15 @@
 #' @export
 # calculate variance explained (R^2) for a genetic variant
-R2 <- function(beta = BETA_exposure, eaf = EAF_exposure, var = 1){
-  out <- (2*eaf*(1 - eaf)*beta^2)/var
+R2 <- function(beta = BETA_exposure, eaf = EAF_exposure, var = 1, binary = FALSE){
+  
+  ve <- pi^2/3
+  
+  if(binary){
+    vg <- 2*eaf*(1 - eaf)*beta^2
+    out <- vg / (vg + ve)
+  } else {
+    out <- (2*eaf*(1 - eaf)*beta^2)/var
+  }
+  
   return(out)
 }
