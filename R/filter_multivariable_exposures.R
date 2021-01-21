9df449720ad68dcc55d3f29cd5e680b03ab6dfb9
vkarhune <vkarhune@gmail.com>
2021-01-21 12:26:00 +0000

    Fix filter_multivariable_exposures

--- R/filter_multivariable_exposures.R
+++ R/filter_multivariable_exposures.R
@@ -1,10 +1,12 @@
 #' @export
 # filter exposures for multivariable MR
-filter_multivariable_exposures <- function(d_exposures, pthresh){
+filter_multivariable_exposures <- function(d_exposures, pthresh, snps = NULL){
   
+  if(is.null(snps)){
   snps <- unlist(lapply(d_exposures, function(x){
     out <- x[pval < pthresh,][["rsid"]]
   }))
+  }
   
   cat(sprintf("Number of SNPs for instruments: %s\n", length(snps)))
   
