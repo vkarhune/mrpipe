e78f6288838768445718231e95542a6b423d8c8d
vkarhune <vkarhune@gmail.com>
2021-02-01 16:16:13 +0000

    Minor fixes

--- R/exclude_palindromic.R
+++ R/exclude_palindromic.R
@@ -4,8 +4,8 @@ exclude_palindromic <- function(d_input = NULL, mafthresh = 0.4){
   
   # remove palindromic with MAF
   d_out <- d_input[!((
-    paste0(EA_exposure, NEA_exposure) %in% c("AT", "TA", "CG", "GC") & MAF_exposure > 0.4) & 
-      (paste0(EA_outcome, NEA_outcome) %in% c("AT", "TA", "CG", "GC") & MAF_outcome > 0.4)),]
+    paste0(EA_exposure, NEA_exposure) %in% c("AT", "TA", "CG", "GC") & EAF_exposure_aligned > 0.4) & 
+      (paste0(EA_outcome, NEA_outcome) %in% c("AT", "TA", "CG", "GC") & EAF_outcome_aligned > 0.4)),]
   
   return(d_out)
 }
