075623e921d7c6de776223d4faa235dd1a334ca5
vkarhune <vkarhune@gmail.com>
2021-02-03 09:28:39 +0000

    Fix remove_flipped()

--- R/remove_flipped.R
+++ R/remove_flipped.R
@@ -1,6 +1,15 @@
 #' @export
 # remove flipped variants
 remove_flipped <- function(d_input = NULL){
-  d_out <- d_input[EA_exposure_aligned == EA_outcome_aligned,]
+  # d_out <- d_input[EA_exposure_aligned == EA_outcome_aligned,]
+  
+  # removing those that have flipped:
+  d_out <- d_input[!(EA_exposure_aligned == NEA_outcome_aligned),]
+  
+  # retain those that may be strand flips
+  d_out <- d_out[(EA_exposure_aligned == EA_outcome_aligned) |
+                   (paste0(EA_exposure_aligned, EA_outcome_aligned) %in% c("AT", "TA", "CG", "GC") &
+                      NEA_exposure_aligned == NEA_outcome_aligned),]
+  
   return(d_out)
 }
