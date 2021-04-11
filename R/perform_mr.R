3df354cc8e3f1e2255fd5757caa9576e1dcec827
vkarhune <vkarhune@gmail.com>
2021-04-11 11:46:29 +0300

    Add options for fixed-effects and unweighted models

--- R/perform_mr.R
+++ R/perform_mr.R
@@ -2,7 +2,8 @@
 # performs MR and creates output
 
 perform_mr <- function(d_input = d_mr, mr_input_data = NULL, output_name = NULL,
-                       expo = expo, outcome = outcome, save = TRUE){
+                       expo = expo, outcome = outcome, save = TRUE, FE = FALSE,
+                       unweighted = FALSE){
   if(is.null(mr_input_data)){
   mr_input_data <- MendelianRandomization::mr_input(
     bx = d_input$BETA_exposure,
@@ -18,18 +19,27 @@ perform_mr <- function(d_input = d_mr, mr_input_data = NULL, output_name = NULL,
     (res_ivwr <- MendelianRandomization::mr_ivw(mr_input_data, model = "random"))
   } else { cat(sprintf("No proxies found for MR of %s on %s\n", expo, outcome))}
 
+  if(FE){
+    res_ivw <- MendelianRandomization::mr_ivw(mr_input_data, model = "fixed")
+  }
+  
   if(dim(d_input)[1] > 2){
     (res_egger <- MendelianRandomization::mr_egger(mr_input_data))
     (res_wm <- MendelianRandomization::mr_median(mr_input_data, weighting = "weighted", seed = 123))
     (res_wmode <- MendelianRandomization::mr_mbe(mr_input_data, weighting = "weighted", seed = 123))
+  }
+  
+  if(unweighted){
+    res_m <- MendelianRandomization::mr_median(mr_input_data, weighting = "simple", seed = 123)
+    res_mode <- MendelianRandomization::mr_mbe(mr_input_data, weighting = "simple", seed = 123)
   }
 
   ### output results
   data_frame <- d_input
 
   cat(sprintf("Saving objects\n"))
-  # ADD HERE IF NEEDED
-  objects_to_save <- ls()[ls() %in% c("d_input", "res_ivwr", "res_egger", "res_wm", "res_wmode")]
+  objects_to_save <- ls()[ls() %in% c("d_input", "res_ivwr", "res_egger", "res_wm", "res_wmode",
+                                      "res_ivw", "res_m", "res_mode")]
 
   # output <- paste0(c(expo, args[-1]), collapse="_")
 
